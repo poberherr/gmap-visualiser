ec4b2ae19c7d3d4bfcec9038c2bd8185459d9d4f
poberherr <patrick.oberherr@gmail.com>
2015-01-11 03:35:25 +0100

    Adding comments for doccer

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//app.use(express.static(__dirname)); // Current directory is root
-app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+// Here we set the folder what servers as the main root folder
+// for our URL
+// "public" off of current is root
+app.use(express.static(path.join(__dirname, 'public')));
 
 port = 3000;
 app.listen(port);
